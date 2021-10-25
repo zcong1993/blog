3f093b643d5b2a1c24846cf65c0281231598d2a5
zcong1993 <zhangcong1992@gmail.com>
2021-10-25 14:55:19 +0800

    ci: auto sync to gitee

--- tools/complete-image-url.js
+++ tools/complete-image-url.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 const argv = process.argv.slice(2)
 
-const PUBLIC_URL = 'https://blog.cong.moe'
+const PUBLIC_URL = 'https://gitee.com/zcong1993/blog/raw/master/static'
 
 if (argv.length !== 1) {
   console.log('need one arg for process file!')
